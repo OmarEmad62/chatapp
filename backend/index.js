5cc1fd5c68723ce6416455343afbf90a84727202
OmarEmad62 <omar.emad.eldin52@gmail.com>
2025-05-14 22:37:26 +0300

    added in the backend env

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: process.env.CLIENT_ORIGIN,
     credentials:true
 }))
 app.use("/api/auth",authRouter);
