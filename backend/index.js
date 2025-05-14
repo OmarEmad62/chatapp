bdd4c4bd38dd86981b90385abf27ce8e3f2c2169
OmarEmad62 <omar.emad.eldin52@gmail.com>
2025-05-14 23:13:12 +0300

    changed env

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.CLIENT_ORIGIN,
+    origin:"http://localhost:5173",
     credentials:true
 }))
 app.use("/api/auth",authRouter);
